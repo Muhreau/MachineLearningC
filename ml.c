d01de3755558291b978a15f364a09c0ce3f9fde9
Matthew <matthewmoreau16@gmail.com>
2024-05-28 21:03:49 -0700

    fuck this compiler and this language

--- ml.c
+++ ml.c
@@ -1,3 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
+
+float train[][2] = {
+
+    {0,0},
+    {1,2},  
+    {2,4},
+    {3,6},
+    {4,8},
+    {5,10},
+
+};
+
+float rand_float(void){
+
+    return (float) rand()/ (float) RAND_MAX;
+}
+int main()
+{
+    srand(time(0));
+    float crack = rand_float();
+    fflush("%f\n", crack);
+    return 0;
+}
